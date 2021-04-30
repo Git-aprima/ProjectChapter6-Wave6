110bd85e707b84bcc7dd48d241945b8ff7999d1e
irinalaifa <irinalaifa@gmail.com>
2021-04-30 14:22:27 +0800

    Revert "chatbot layout"
    This reverts commit debdd2dd0538282f4a4ee496490bbc7421e800cf.

--- app/src/main/java/com/example/batuguntingkertas/utils/KonteksPesan.kt
+++
@@ -1,9 +0,0 @@
-package com.example.batuguntingkertas.utils
-
-object KonteksPesan {
-
-    const val ID_KIRIM = "ID_KIRIM"
-    const val ID_TERIMA = "ID_TERIMA"
-
-    const val OPEN_SEARCH = "Searching..."
-}
