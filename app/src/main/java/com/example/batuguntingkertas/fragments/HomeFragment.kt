a33a17fad20a155af8a593c80961658fdcb0d9ac
rendy-byte <rendysetiawanadisaputra@gmail.com>
2021-04-30 11:34:08 +0700

    no message

--- app/src/main/java/com/example/batuguntingkertas/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/batuguntingkertas/fragments/HomeFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.example.batuguntingkertas.R
 
 
 class HomeFragment : Fragment() {
+    private lateinit var icon:ImageView
 
 
     override fun onCreateView(
@@ -17,6 +20,17 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Permission Gilde
+        icon = view.findViewById(R.id.icon)
+        Glide.with(this)
+            .load("https://i.ibb.co/HC5ZPgD/splash-screen%201.png")
+            .into(icon)
+
     }
 
 }
--- app/src/main/java/com/example/batuguntingkertas/fragments/MemberFragment.kt
+++ app/src/main/java/com/example/batuguntingkertas/fragments/MemberFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.example.batuguntingkertas.R
 
 class MemberFragment : Fragment() {
+    private lateinit var icWellcome: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +18,17 @@ class MemberFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_member, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Permission Gilde
+        icWellcome = view.findViewById(R.id.icWelcome)
+        Glide.with(this)
+            .load("https://www.freepnglogos.com/uploads/welcome-png/wonderful-welcome-images-18.png")
+            .into(icWellcome)
+
     }
 
 
