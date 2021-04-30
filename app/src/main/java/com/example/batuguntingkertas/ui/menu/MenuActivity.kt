7d3a2b5d04be3cd52781bc1d55d21afe8485d787
fery123321 <ferryfebruari@gmail.com>
2021-05-01 04:51:25 +0700

    Update layout XML, menambahkan menu navigation component

---
+++ app/src/main/java/com/example/batuguntingkertas/ui/menu/MenuActivity.kt
@@ -0,0 +1,19 @@
+package com.example.batuguntingkertas.ui.menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.batuguntingkertas.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        val navController = findNavController(R.id.fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnvMain)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/com/example/batuguntingkertas/ui/play/VsCpu.kt
+++ app/src/main/java/com/example/batuguntingkertas/ui/play/VsCpu.kt
@@ -1,12 +1,12 @@
-package com.example.batuguntingkertas.scoreHistory
+package com.example.batuguntingkertas.ui.play
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.batuguntingkertas.R
 
-class ScoreHistoryActivity : AppCompatActivity() {
+class VsCpu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_score_history)
+        setContentView(R.layout.activity_vs_cpu)
     }
 }
