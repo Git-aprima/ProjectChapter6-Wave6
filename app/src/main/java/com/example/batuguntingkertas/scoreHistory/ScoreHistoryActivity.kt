2faae73f28d1613bd3e69ae832a7b607fa5d6f7b
DESKTOP-NQKD8M3\pinem <pinemta82@gmail.com>
2021-04-30 19:16:05 +0700

    buat feature history

---
+++ app/src/main/java/com/example/batuguntingkertas/scoreHistory/ScoreHistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.batuguntingkertas.scoreHistory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.batuguntingkertas.R
+
+class ScoreHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score_history)
+    }
+}
